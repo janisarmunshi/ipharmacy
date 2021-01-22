c85334b4b4cb682eb4e8b2946b3d680644203708
janisarmunshi <janisar.munshi@gmail.com>
2021-01-22 23:05:19 +0530

    a

--- ipharmacy/settings.py
+++ ipharmacy/settings.py
@@ -129,9 +129,10 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'staticdir')
+# ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
